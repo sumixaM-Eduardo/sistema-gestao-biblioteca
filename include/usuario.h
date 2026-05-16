4d57610177e929b6d7296d0e653a900c188428aa
Maximus <maximus.cruz@penedo.ufal.br>
2026-05-15 21:37:19 -0300

    CRUD USUARIOS

---
+++ include/usuario.h
@@ -0,0 +1,13 @@
+#ifndef USUARIOS_H
+#define USUARIOS_H
+
+typedef struct {
+    char name[50];
+    char password[50];
+    char username[50];
+    int id;
+    int type;
+    int active;
+}Usuario;
+
+#endif
